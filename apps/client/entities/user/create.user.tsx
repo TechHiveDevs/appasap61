f456dd69ad75947e8826581e63a1195271a57e74
TechHiveDevs <techhivedevs@gmail.com>
2022-11-08 13:57:42 +0000

    inital auto commit

---
+++ apps/client/entities/user/create.user.tsx
@@ -0,0 +1,27 @@
+
+import { 
+  Create,
+  SimpleForm,
+  
+required,
+TextInput,
+AutocompleteInput,
+} from "react-admin";
+
+// ------------------------------------------------
+
+export default function CreateUser(props: any) {
+    return (
+      <Create>
+        <SimpleForm redirect="list">
+          <TextInput source="name" variant="outlined" validate={required()}  />
+<TextInput source="email" variant="outlined" validate={required()}  />
+<AutocompleteInput variant="outlined" source="gender" choices={[
+{ id: "Male", name: "Male" },
+{ id: "Female", name: "Female" },
+]}  />
+
+        </SimpleForm>
+      </Create>
+    );
+  }
---
+++ apps/client/entities/user/show.user.tsx
@@ -0,0 +1,26 @@
+
+import { 
+  Show,
+  SimpleShowLayout,
+  
+NumberField,
+DateField,
+TextField,
+} from "react-admin";
+
+// ------------------------------------------------
+
+export default function ShowUser(props: any) {
+return (
+    <Show>
+      <SimpleShowLayout>
+        <NumberField source="id" />
+<TextField source="name" />
+<TextField source="email" />
+<TextField source="gender" />
+<DateField source="createdAt" />
+
+      </SimpleShowLayout>
+    </Show>
+  );
+}
